1e37ad6d43c0198cbfe33a42de57c743d72c2d59
Tod Hansmann <githubpub@todandlorna.com>
2024-06-09 14:55:08 -0600

    feat: parsing config and overriding defaults

---
+++ src/pysocha/config.py
@@ -0,0 +1,35 @@
+import yaml
+
+default_yaml = """
+contentDir: "content"
+outputDir: "generated"
+templatesDir: "templates"
+siteTitle: "Example dot Com!"
+descending: True
+defaultExtension: ".html"
+startPage: "index.html"
+pageConfig: 
+  pageTitle: "Example dot Com!"
+  pageDefaultTemplate: "page.jinja2"
+blogConfig: 
+  title: "Tod and Lorna blog"
+  blogBaseDir: "tnlblog"
+  blogTemplate: "blog.jinja2"
+  listingTemplate: "listing.jinja2"
+  listingPagination_num: 5
+  listingKeysNeeded: ['hook']
+  atomFeed: True
+  tagsTemplate: "tags.jinja2"
+  tagTemplate: "tag.jinja2"
+  tagPaginationNum: 5
+  authorsTemplate: "authors.jinja2"
+  authorTemplate: "author.jinja2"
+  authorPaginationNum: 10
+"""
+
+CONFIG = yaml.load(default_yaml)
+
+def override_config(overrides: dict) -> dict:
+    CONFIG.update(overrides)
+    return CONFIG
+
